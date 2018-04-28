ac1abb61e3e19419bb197193da837847099dc5f4
Andre Gavazzoni <andre.gavazzoni@gmail.com>
2018-04-28 11:04:57 -0300

    Firebase and form

---
+++ Assets/SignUpFormHandler.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SignUpFormHandler : MonoBehaviour
+{
+
+    private Firebase.Auth.FirebaseAuth _auth;
+
+    private InputField UsernameInput;
+    private InputField PasswordInput;
+
+    private void Awake()
+    {
+        
+    }
+
+    public void SetUsername(InputField username) {
+        UsernameInput = username;
+    }
+
+    public void SetPassword(InputField password)
+    {
+        PasswordInput = password;
+    }
+
+    public void SaveUser()
+    {
+        Debug.Log(UsernameInput.text);
+        Debug.Log(PasswordInput.text);
+
+        var auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        auth.CreateUserWithEmailAndPasswordAsync(UsernameInput.text, PasswordInput.text).ContinueWith(task =>
+        {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + task.Exception);
+                return;
+            }
+
+            // Firebase user has been created.
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("Firebase user created successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+        });
+    }
+}
